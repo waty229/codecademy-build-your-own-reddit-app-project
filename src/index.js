3dfa4975ae819c9d590c1bc28f371112b180824f
shaun watkins <swatkins229@outlook.com>
2024-10-08 12:46:06 +0100

    updated to createRoot

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import store from './store';
+import store from '../src/components/store';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <Provider store={store}>
     <Router>
       <Routes>
@@ -14,6 +17,5 @@ ReactDOM.render(
         {/* Add more routes here */}
       </Routes>
     </Router>
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
